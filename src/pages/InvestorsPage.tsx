d8f8aacbbabae4e031ed860b10ecedc7b275581c
ugobe007 <ugobe07@gmail.com>
2025-11-02 00:28:58 -0700

    Make investor cards more compact - 4 cards fit horizontally on large screens

--- src/pages/InvestorsPage.tsx
+++ src/pages/InvestorsPage.tsx
@@ -212,7 +212,7 @@ export default function InvestorsPage() {
 
         {/* Investor Grid */}
         {filteredInvestors.length > 0 ? (
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
             {filteredInvestors.map((investor) => (
               <InvestorCard
                 key={investor.id}
